59d63561d72a72c6cd26e1adf213c336e317d4b7
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-07-30 14:52:10 +0200

    .

--- InForno/InForno/Models/Checkout.cs
+++ InForno/InForno/Models/Checkout.cs
@@ -3,7 +3,7 @@
     public class Checkout
     {
         public int CheckoutId { get; set; }
-        public List<Product> Products { get; set; }
+        public List<Order> Orders { get; set; }
         public string Address { get; set; }
         public string Note { get; set; }
         public DateTime DateTime { get; set; }
