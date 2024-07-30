9bc91fa973aacaffe6923ac53b5fc9d29260217f
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-07-30 15:10:21 +0200

    .

---
+++ InForno/InForno/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InForno.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+    }
+}
