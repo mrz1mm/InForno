0e9d8c8b0153cd24a7356e2ae1ab89dfab73d492
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-07-30 16:21:43 +0200

    .

--- InForno/InForno/Controllers/AuthController.cs
+++ InForno/InForno/Controllers/AuthController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InForno.Models;
+using InForno.Models.DTO;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace InForno.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly InFornoDbContext _context;
+        public AuthController(InFornoDbContext context)
+        {
+            _context = context;
+        }
+
+        // CREAZIONI VISTE
         public IActionResult Login()
         {
             return View();
@@ -15,6 +28,65 @@ namespace InForno.Controllers
         }
 
 
+        // METODI
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
 
+            var user = new User
+            {
+                Username = model.Username,
+                Password = model.Password,
+                Role = model.Role
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Login");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Username,Password")] LoginDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Errore nei dati inseriti";
+                return View();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username == model.Username && u.Password == model.Password);
+
+            if (user == null)
+            {
+                TempData["error"] = "Account non esistente";
+                return View();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+            TempData["Success"] = "Login effettuato con successo";
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
